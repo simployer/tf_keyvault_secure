06e938dd56b3109c998ac902c602c726bfcd186d
martinhelgesen <martin.helgesen@simployer.com>
2023-02-06 10:26:14 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "kv" {
+  name                     = "kv-${var.NAME}"
+  location                 = var.LOCATION
+  resource_group_name      = var.RESOURCE_GROUP_NAME
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  sku_name                 = "standard"
+  access_policy {
+    object_id = data.azurerm_client_config.current.object_id
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    secret_permissions = [
+      "Get",
+    ]
+  }
+  network_acls {
+    bypass         = "None"
+    default_action = "Deny"
+    ip_rules = module.common.internal_gateway_ips
+    virtual_network_subnet_ids = module.common.buildsubnets
+  }
+  tags = var.TAGS
+}
+
+module "common" {
+  source      = "git::http://simployer:simployer@gitserver.simployer.tech/terraform-modules-common.git"
+  project     = var.PROJECT
+  environment = var.ENVIRONMENT
+}
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "NAME" {
+  default = ""
+}
+variable "LOCATION" {
+  default = ""
+}
+variable "RESOURCE_GROUP_NAME" {
+  default = ""
+}
+variable "TAGS" {
+  default = ""
+}
+variable "PROJECT" {
+  default = ""
+}
+variable "ENVIRONMENT" {
+  default = ""
+}
